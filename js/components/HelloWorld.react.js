e7cdeba66e2f2c050a1b086e5d02b618564cd1c0
Adrian Kosciak <adrian.kosciak@parivedasolutions.com>
2015-12-17 16:47:32 -0600

    updated

---
+++ js/components/HelloWorld.react.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({displayName: "HelloWorld",
+	render: function(){
+		return (
+			<div> 
+				Hello World !
+			</div>
+		);
+	}
+});
+
+module.exports = HelloWorld;
